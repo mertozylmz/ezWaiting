2667c88e74c4fae846eb9227e1b8bbd09f035aaa
mertozyilmaz <mertozyilmazz@gmail.com>
2020-10-21 12:37:55 +0300

    create js fixed

--- api/controllers/admin/title/create.js
+++ api/controllers/admin/title/create.js
@@ -8,7 +8,21 @@ module.exports = {
 
 
   inputs: {
-
+    name: {
+      type: 'string'
+    },
+    rating: {
+      type: 'string'
+    },
+    publisher: {
+      type: 'ref'
+    },
+    country: {
+      type: 'ref'
+    },
+    categories: {
+      type: 'ref'
+    }
   },
 
 
@@ -18,10 +32,24 @@ module.exports = {
 
 
   fn: async function (inputs) {
+    try {
+      let res = this.res;
 
-    // All done.
-    return;
+      let title = await Title.create({
+        name: inputs.name,
+        rating: inputs.rating,
+        publisher: inputs.publisher,
+        country: inputs.country,
+        categories: inputs.categories
+      }).fetch();
 
+      return res.redirect('/title/update/' + title.id);
+    } catch (error) {
+      console.log("Create title error: ", error);
+      return exits.invalidRequest({
+        message: "Oops a problem occured.",
+      });
+    }
   }
 
 
