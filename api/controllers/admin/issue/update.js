546e15f748a680a2271eaa23e8b0d1e064bb76e1
mertozyilmaz <mertozyilmazz@gmail.com>
2020-10-21 12:40:43 +0300

    update issue js fixed

--- api/controllers/admin/issue/update.js
+++ api/controllers/admin/issue/update.js
@@ -8,20 +8,54 @@ module.exports = {
 
 
   inputs: {
-
+    name: {
+      type: "string",
+      required: true
+    },
+    issueLiveDate: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string"
+    },
+    status: {
+      type: "boolean"
+    },
+    title: {
+      type: "ref"
+    }
   },
 
 
   exits: {
-
+    invalidRequest: {
+      statusCode: 500
+    }
   },
 
 
-  fn: async function (inputs) {
+  fn: async function (inputs, exits) {
+    try {
+      let res = this.res;
+      let req = this.req;
 
-    // All done.
-    return;
+      await Issue.updateOne({ id: req.param('id') }).set({
+        name: inputs.name,
+        issueLiveDate: inputs.issueLiveDate,
+        description: inputs.description,
+        status: inputs.status,
+        title: inputs.title
+      });
 
+      return res.redirect("/admin/issue/update/" + req.param('id'));
+
+    } catch (error) {
+      console.log("Create issue error: ", error);
+      return exits.invalidRequest({
+        message: "Oops a problem occured.",
+      });
+    }
   }
 
 
