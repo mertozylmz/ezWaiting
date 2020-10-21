b444a76add27a7bed275a5a86bee526a7c9d7df3
mertozyilmaz <mertozyilmazz@gmail.com>
2020-10-21 12:46:33 +0300

    title update fixed

--- api/controllers/admin/title/update.js
+++ api/controllers/admin/title/update.js
@@ -8,20 +8,52 @@ module.exports = {
 
 
   inputs: {
-
+    name: {
+      type: 'string'
+    },
+    rating: {
+      type: 'string'
+    },
+    publisher: {
+      type: 'ref'
+    },
+    country: {
+      type: 'ref'
+    },
+    categories: {
+      type: 'ref'
+    }
   },
 
 
   exits: {
-
+    invalidRequest: {
+      statusCode: 500
+    }
   },
 
 
-  fn: async function (inputs) {
+  fn: async function (inputs, exits) {
+    try {
+      let res = this.res;
+      let req = this.req;
 
-    // All done.
-    return;
+      await Title.updateOne({ id: req.param('id') }).set({
+        name: inputs.name,
+        rating: inputs.rating,
+        publisher: inputs.publisher,
+        country: inputs.country,
+        categories: inputs.categories
+      });
 
+      return res.redirect("/admin/title/update/" + req.param('id'));
+
+    } catch (error) {
+      console.log("Create title error: ", error);
+      return exits.invalidRequest({
+        message: "Oops a problem occured.",
+      });
+    }
   }
 
 
