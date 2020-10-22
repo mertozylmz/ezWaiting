6dd0a8673aba19c8603073c0716958a00d80cffb
Ali Göklükay <aligoklukay@Ali-MacBook.home>
2020-10-22 12:39:36 +0300

    Db

--- config/env/production.js
+++ config/env/production.js
@@ -47,8 +47,8 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     default: {
-      // adapter: 'sails-mysql',
-      // url: 'mysql://user:password@host:port/database',
+      adapter: 'sails-mongo',
+      url: 'mongodb+srv://ezwaiting:123qwe4Ez!@ezwaiting.e3p2m.mongodb.net/ezwaiting_db?retryWrites=true&w=majority'
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
