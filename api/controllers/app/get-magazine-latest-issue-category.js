c49f21bcda52123fc968b6596d8cb869b96bd62f
Ali Göklükay <aligoklukay@gmail.com>
2020-11-07 23:34:55 +0300

    Fix try

--- api/controllers/app/get-magazine-latest-issue-category.js
+++ api/controllers/app/get-magazine-latest-issue-category.js
@@ -28,15 +28,18 @@ module.exports = {
         where: {
           isActive: true,
           isDeleted: false,
-          category:category
         },
         skip: (req.param("page") - 1) * limit,
         limit: limit,
-      }).populate("issues");
+      }).populate("issues").populate('categories');
 
       let titleHasOneIssue = allTitles.filter((title) => {
-        return title.issues.length > 0;
+        if(category == title.categories.id && title.issues.length > 0){
+          return title.issues
+        }
       });
+
+      console.log(titleHasOneIssue);
 
       let issues = titleHasOneIssue.map((title) => {
         let issue = title.issues.pop();
