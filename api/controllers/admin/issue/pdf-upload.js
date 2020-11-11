42469f3863376ba36ab50ef3666be89bf4b08afb
Ali Göklükay <aligoklukay@gmail.com>
2020-11-11 23:50:50 +0300

    Fix page

--- api/controllers/admin/issue/pdf-upload.js
+++ api/controllers/admin/issue/pdf-upload.js
@@ -48,7 +48,7 @@ module.exports = {
         {
           adapter: require("skipper-azure"),
           key: "ezwaitingroomv2",
-          secret: "M9pwOy08NL0dMdlHUiqy569n2ffp0JAuSVIVpMbC577dEFGESZaQbuWr45TpyhTPkK+dxVr0fofhqXR9HKjxZA==",
+          secret: "",
           container: "magazinefiles",
           dirname: directoryName,
           maxBytes: 10000000000,
