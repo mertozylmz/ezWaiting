5f67241c95160a3a8db4c06b564d5e27f5f306c0
Ali Göklükay <aligoklukay@gmail.com>
2020-11-11 18:58:10 +0300

    Fix page

--- api/controllers/app/get-carousel-magazines-issues.js
+++ api/controllers/app/get-carousel-magazines-issues.js
@@ -49,7 +49,7 @@ module.exports = {
           id: issue.id,
           name: issue.name,
           issueDescription: issue.description,
-          thumbImg: (pdf.thumbImg) ? pdf.thumbImg:null ,
+          thumbImg: (pdf) ? pdf.thumbImg : null ,
         };
       });
 
